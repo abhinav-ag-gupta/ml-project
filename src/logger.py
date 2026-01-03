30ae665ee752102cb6f43781b78daaabc81beb4d
abhinav-ag-gupta <abhinavgta18@gmail.com>
2026-01-03 13:44:31 +0530

    Project Structure || Logging & Exception

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+from exception import CustomException
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOGS_PATH = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(LOGS_PATH, exist_ok=True)
+
+LOGS_FILE_PATH = os.path.join(LOGS_PATH,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOGS_FILE_PATH,
+    format="[%(asctime)s-%(lineno)d-%(name)s-%(levelname)s-%(message)s]",
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO,
+)
+
